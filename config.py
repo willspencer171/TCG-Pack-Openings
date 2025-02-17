bc668b8108d794956a8b5f4e4bdf2485295566cd
Will Spencer <willspencer171@gmail.com>
2025-02-17 14:10:22 +0000

    Configured RestClient

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+from pokemontcgsdk import RestClient
 
 load_dotenv()
 
@@ -7,6 +8,8 @@ API_KEY = os.getenv('API_KEY')
 
 if not API_KEY:
     raise ValueError("API_KEY is missing! Please check .env file")
+
+RestClient.configure(API_KEY)
 
 PACK_SIZE = 10
 
