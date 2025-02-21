382cf5bf3544d241951bbed7b44e3c33334d859b
Will Spencer <willspencer171@gmail.com>
2025-02-21 21:12:15 +0000

    Added test for set list location

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,10 @@
 import config
+import os
 from pokemontcgsdk import RestClient
 
 def test_api_key_registered():
     assert RestClient.api_key == config.API_KEY
+
+def test_setlist_location():
+    assert (os.path.exists(config.SET_LIST_LOCATION), 
+            'Please set \'SET_LIST_LOCATION\' environment variable')
