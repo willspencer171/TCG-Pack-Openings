c817a203f0d07b14b720d2e6488acff8b95702b5
Will Spencer <willspencer171@gmail.com>
2025-02-21 21:15:20 +0000

    Setup pack generation for a random set. Helped find anomalies in certain series regarding energy cards

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,23 @@
 import pytest
+from config import SET_LIST_LOCATION
+from random import choice
+from pokemontcgsdk import Set
 from src.pack_logic import Pack
+import os
 
 @pytest.fixture(scope='session')
 def pack_for_testing():
-    """Enter set code here to choose which set the pack will be drawn from"""
-    return Pack('sv8')
+    """Set code is picked at random from a list of sets. 
+    This minimises API calls but requires updating when new sets release"""
+    setlist_path = SET_LIST_LOCATION
+    if not os.path.exists(setlist_path):
+        packs = [set.id for set in Set.all()]
+        with open(setlist_path, 'x') as f:
+            f.write("\n".join(packs))
+
+    with open(SET_LIST_LOCATION, 'r') as f:
+        pack_ids = f.readlines()
+
+    chosen_pack = choice(pack_ids)
+    
+    return Pack(chosen_pack)
