88abd23b6364f19e4e9527ccd1e8308830805e57
Will Spencer <willspencer171@gmail.com>
2025-05-07 17:51:19 +0100

    Lots of new stuff here too. Can get images for cards in a pack. Can pull 10 packs at once. Changed a few bits for getting the right energy cards, some more debugging info. Sets that have a trainer gallery (swsh9 - swsh12pt5) have those incorporated. Won't be including the galleries in set pickers. HashCard now has price and quality attributes which is cool for picking a holo or reverse or whatever. Also for 1st editions :)

--- src/pack_logic.py
+++ src/pack_logic.py
@@ -1,8 +1,7 @@
-from __future__ import annotations
-from typing import Self
+from __future__ import annotations # Deprecated in Python 3.14
 
-from src.utils import debug_message
-from config import RARITY_RANKING
+from src.utils import debug_message, download_pack_images
+from config import RARITY_RANKING, APP_CURRENCY, CURRENCY_CONVERSIONS, setlist
 
 import random
 
@@ -13,14 +12,18 @@ from dataclasses import dataclass
 class HashCard(Card):
     """HashCard dataclass ensures a hash table-like object can be used to
     keep track of obtained cards from pulls in the future"""
+
+    quality: str
+    qualities = ['normal', 'holofoil', 'reverseHolofoil', '1stEditionNormal', '1stEditionHolofoil']
+
     def __hash__(self):
-        return hash(self.id)
+        return hash(self.id + self.quality)
     
     def __eq__(self, other):
         return self.id == other.id
     
     @staticmethod
-    def find(id) -> Self:
+    def find(id) -> HashCard:
         return QueryBuilder(HashCard, HashCard.transform).find(id)
     
     @staticmethod
@@ -28,16 +31,33 @@ class HashCard(Card):
         return QueryBuilder(HashCard, HashCard.transform).where(**kwargs)
 
     @staticmethod
-    def all() -> list[Self]:
+    def all() -> list[HashCard]:
         return QueryBuilder(HashCard, HashCard.transform).all()
     
     @staticmethod
-    def transform(response):
-        if response.get('tcgplayer', {}).get('prices', {}).get('1stEditionNormal'):
-            response['tcgplayer']['prices']['firstEditionNormal'] = response['tcgplayer']['prices'].pop('1stEditionNormal')
-        if response.get('tcgplayer', {}).get('prices', {}).get('1stEditionHolofoil'):
-            response['tcgplayer']['prices']['firstEditionHolofoil'] = response['tcgplayer']['prices'].pop('1stEditionHolofoil')
+    def transform(response: dict):
+        this_quals = []
+        for qual in HashCard.qualities:
+            if response.get('tcgplayer', {}).get('prices', {}).get(qual):
+                response['tcgplayer']['prices'][qual] = {key: round(val * CURRENCY_CONVERSIONS[APP_CURRENCY], 2) 
+                                                          for key, val in 
+                                                          response['tcgplayer']['prices'].pop(qual).items()
+                                                          if val}
+                this_quals.append(qual)
+        
+        if len(this_quals) != 0:
+            response['quality'] = random.choices(this_quals, weights=list(range(len(this_quals), 0, -1)))[0]
+        else:
+            response['quality'] = response.get('quality', 'normal')
         return response
+    
+    @property
+    def price(self):
+        return getattr(getattr(getattr(getattr(
+            self, 'tcgplayer', None), 
+            'prices', None), 
+            self.quality, None), 
+            'market', 0.0)
 
 class Pack:
     ### Should be an abstract class implemented by each set? Maybe?
@@ -48,28 +68,50 @@ class Pack:
     # 9 Other cards
     # Last card is guaranteed at least rare (rank 5)
 
-    def __init__(self, set_id):
+    def __init__(self, set_id, ten_pack=False):
         self.set_id = set_id
-        self.set = Set.find(set_id)
-        self.pack_items: list[HashCard | None] = []
+        self.ten_pack = ten_pack
+        self.set: Set = Set.find(set_id)
+        self.pack_items: list[HashCard | None]
+        self.available: list[HashCard | None]
         self.pick_cards()
+    
+    def __str__(self):
+        return f'{self.set.name}'
 
     def pick_cards(self):
         debug_message(f"Fetching cards from {self.set.name}...")
-        available = HashCard.where(q=f'set.id:{self.set_id}')
-        debug_message("Set cards retrieved!")
-        high_rarity = [card for card in available if
-                       RARITY_RANKING[card.rarity] >= 7]
-
-        energies = [card for card in available if 
+        self.available = HashCard.where(q=f'set.id:{self.set_id} rarity:*')
+        if len(self.available) != 0:
+            if any([s in [self.set.id + "gg", self.set.id + "tg"] for s in setlist]):
+                debug_message('Adding gallery set')
+                self.available += HashCard.where(q=f'set.id:{self.set_id}*g rarity:*')
+            debug_message("Set cards retrieved!")
+            high_rarity = [card for card in self.available if
+                       RARITY_RANKING[card.rarity] >= 5]
+        else:
+            debug_message('Set has no rarity info, probs a promo')
+            self.available = HashCard.where(q=f'set.id:{self.set_id}')
+            high_rarity = self.available
+        
+        energies = [card for card in self.available if 
                     card.supertype == 'energy'
                     and 'basic' in card.subtypes and
                     not any(rarity in card.rarity for 
-                            rarity in ['secret', 'hyper'])]
+                            rarity in ['secret', 'hyper', None])]
         
         for card in energies:
-            if card in available:
-                available.remove(card)
+            if card in self.available:
+                self.available.remove(card)
+
+        if self.ten_pack:
+            n_standard = 80
+            n_high = 10
+            n_energy = 10
+        else:
+            n_standard = 8
+            n_high = 1
+            n_energy = 1
 
         if len(energies) == 0:
             debug_message("No basic Energies in set, looking in series")
@@ -92,13 +134,15 @@ class Pack:
                     case "Platinum":
                         energies = HashCard.where(q=(f'set.series:"Diamond & Pearl" supertype:energy subtypes:basic'))
 
+        debug_message(f'Got {len(energies)} energies')
         # Add Energy card for first card
-        card = random.choice(energies)
-        self.pack_items.append(card)
+        self.pack_items = random.choices(energies, k=n_energy)
 
+        debug_message(f'Got {len(self.available)} cards, with {len(high_rarity)} high rank')
         # Pick 9 cards
-        self.pack_items = self.pack_items + sorted(Pack._draw_random(available, 8) + Pack._draw_random(high_rarity), 
-                                                   key=lambda x: RARITY_RANKING[x.rarity])
+        self.pack_items = self.pack_items + sorted(Pack._draw_random(self.available, n_standard) + Pack._draw_random(high_rarity, n_high), 
+                                                   key=lambda x: RARITY_RANKING[x.rarity] + 
+                                                                 HashCard.qualities.index(x.quality))
     
     @staticmethod
     def _draw_random(card_pool: list[HashCard], k=1):
@@ -110,6 +154,14 @@ class Pack:
                                k=k)
         return cards
     
+    async def get_pack_images(self):
+        pack_image_urls = [card.images.small for card in self.pack_items]
+        self.image_data = await download_pack_images(pack_image_urls)
+    
+    def print_pack(self):
+        for card in self.pack_items:
+            print(f"{card.name}: {card.rarity}")
+
     def __len__(self):
         return len(self.pack_items)
     
